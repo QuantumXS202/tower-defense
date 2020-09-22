32837433b05d67cc51331a35d45a40500eed95ff
QuantumXS202 <Quinncroese@gmail.com>
2020-09-22 14:32:34 +0200

    Create UnityLockfile

---
+++ Assets/Scripts/Pathfolwwer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEditor;
+using UnityEngine;  
+
+public class Pathfolwwer : MonoBehaviour
+{
+    [SerializeField] Transform[] waypoints; 
+    [SerializeField] private float _speed;
+    
+
+    int waypointIndex = 0;
+
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, _speed * Time.deltaTime);
+        if(transform.position == waypoints [waypointIndex].transform.position)
+        {
+            waypointIndex += 1;
+        }
+
+        if (waypointIndex == waypoints.Length)
+            waypointIndex = 0;
+
+            
+    }
+}
